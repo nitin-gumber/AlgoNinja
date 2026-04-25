fcccc7e9229747a36be906d3ed80c8c5e28f8d97
Nitin Gumber <aroranitin486@gmail.com>
2026-04-25 18:35:54 +0530

    setup config AlgoNinja backend - initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res) => {
+  res.send('Hi, Welocome to AlgoNinja 🔥');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
