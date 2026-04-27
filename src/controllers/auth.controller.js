e5582169bd36252d0a93faa9f65711aa480eb5ec
Nitin Gumber <aroranitin486@gmail.com>
2026-04-27 13:04:59 +0530

    Add Features: user register validation and register controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import { User } from '../models/user.model.js';
-import { bcrypt } from 'bcryptjs';
-import { crypto } from 'crypto';
-import { userRoleEnum } from '../utils/constants';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+import { userRoleEnum } from '../utils/constants.js';
+import { sendMail } from '../utils/sendMail.js';
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const userAlredyExist = await User.findOne({ email: email.lowerCase() });
+    const userAlredyExist = await User.findOne({ email: email.toLocaleLowerCase() });
 
     if (userAlredyExist) {
       return res.status(400).json({
@@ -17,6 +18,7 @@ export const register = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const verficationToken = crypto.randomBytes(32).toString('hex');
     const verificationTokenExpiry = Date.now() + 10 * 60 * 1000; // 10min
 
@@ -29,13 +31,34 @@ export const register = async (req, res) => {
       verificationTokenExpiry: verificationTokenExpiry,
     });
 
-    console.log('User Created Success==>', user);
-
     if (!user) {
       return res.status(400).json({
         success: false,
         message: 'User not regestered',
       });
     }
-  } catch (error) {}
+
+    const emailVerificationLink = `${process.env.BASE_URL}/api/v1/auth/verifyuser/${verficationToken}`;
+
+    await sendMail({
+      to: user.email,
+      subject: 'AlgoNinja Verification Email',
+      message: emailVerificationLink,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: 'User registered successfully. Please check your email to verify your account.',
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user?.image,
+      },
+    });
+  } catch (error) {
+    console.error('Registration Error: ', error);
+    return res.status(500).json({ error: error.message });
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
 import { connectDB } from './utils/dbConnect.js';
+
+import userAuthRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -14,9 +15,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hi, Welocome to AlgoNinja 🔥');
 });
+
+app.use('/api/v1/auth', userAuthRoutes);
 
 (async () => {
   try {
